c304b3489937038011eb111762ffefc4db4ce675
Alexander Reichle-Schmehl <tolimar@debian.org>
2012-03-05 13:31:09 +0000

    Completly redo packaging and prepare new upstream version
    git-svn-id: svn://svn.debian.org/svn/pkg-nagios/nagvis/trunk@2002 98d490a6-b407-0410-aaaa-fdee166ba3f5

---
+++ debian/demo_pool.ini.php
@@ -0,0 +1,17 @@
+; <?php return 1; ?>
+; the line above is to prevent
+; viewing this file from web.
+; DON'T REMOVE IT!
+
+; in this example the browser switches between the maps demo and demo2 every 15
+; seconds, the rotation is enabled by url: index.php?rotation=demo
+[rotation_demo]
+; These steps are rotated. The single steps may have optional prefixes like "Demo2:"
+; which are used as display text on the index pages rotation list.
+; You may also add external URLs as steps. Simply enclose the url using []
+; instead of the map name. It is also possible to add automaps to rotations,
+; add an @ sign before the automap name to add an automap to the rotation.
+maps="demo-germany,demo-ham-racks,demo-load,demo-muc-srv1"
+; rotation interval (seconds)
+interval=5
+
